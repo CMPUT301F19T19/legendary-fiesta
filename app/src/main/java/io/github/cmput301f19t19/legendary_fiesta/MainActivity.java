7879bc1e4d2e73a469834ce35e1c4ede76bf206e
liushuyu <liushuyu011@gmail.com>
2019-10-03 16:21:11 -0600

    meta: initialize Android project structure

---
+++ app/src/main/java/io/github/cmput301f19t19/legendary_fiesta/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.cmput301f19t19.legendary_fiesta;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
