aa3eecff63075f4bc6d6e8848c50c5fe99de5a94
jenyangk <jenyang@ualberta.ca>
2019-10-26 11:18:28 -0600

    Set date on activity result

--- app/src/main/java/io/github/cmput301f19t19/legendary_fiesta/ui/AddPostFragment.java
+++ app/src/main/java/io/github/cmput301f19t19/legendary_fiesta/ui/AddPostFragment.java
@@ -1,5 +1,7 @@
 package io.github.cmput301f19t19.legendary_fiesta.ui;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +10,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import io.github.cmput301f19t19.legendary_fiesta.R;
@@ -25,5 +28,16 @@ public class AddPostFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_add_post, container, false);
         return root;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // Check for the results
+        if (requestCode == DATE_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // Get date from string
+            selectedDate = data.getStringExtra("SELECTED_DATE");
+            // Set date EditText to the selected date
+            dateET.setText(selectedDate);
+        }
     }
 }
