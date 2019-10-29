564326111a8803dbdf175bf40c84fba0ad8dd449
Lefan <tan6@ualberta.ca>
2019-10-28 23:35:52 -0600

    Fixed AddPostFragment code by making it cleaner, madeChanges to edittext

--- app/src/main/java/io/github/cmput301f19t19/legendary_fiesta/ui/AddPostFragment.java
+++ app/src/main/java/io/github/cmput301f19t19/legendary_fiesta/ui/AddPostFragment.java
@@ -3,11 +3,13 @@ package io.github.cmput301f19t19.legendary_fiesta.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,13 +19,11 @@ import androidx.fragment.app.Fragment;
 import io.github.cmput301f19t19.legendary_fiesta.R;
 
 // TODO: Change icon (imageview) to radio buttons
-public class AddPostFragment extends Fragment {
+public class AddPostFragment extends Fragment implements View.OnClickListener{
 
-    private EditText dateET;
-    private EditText timeET;
+    private TextView dateET;
+    private TextView timeET;
     private EditText descET;
-    private Button dateButton;
-    private Button timeButton;
     private Button cancelButton;
     private Button doneButton;
     private String selectedDate;
@@ -39,8 +39,6 @@ public class AddPostFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_add_post, container, false);
 
-        dateButton = root.findViewById(R.id.date_picker_button);
-        timeButton = root.findViewById(R.id.time_picker_button);
         cancelButton = root.findViewById(R.id.cancel_button);
         doneButton = root.findViewById(R.id.done_button);
 
@@ -49,68 +47,14 @@ public class AddPostFragment extends Fragment {
         timeET = root.findViewById(R.id.time_edittext);
         descET = root.findViewById(R.id.description_edittext);
 
-        // Launch DatePicker on Date button press
-        dateButton.setOnClickListener(view -> {
-            // Create DatePickerFragment
-            DialogFragment dateFragment = new DatePickerFragment();
-            // Set the target fragment to receive the results and specifying the result code
-            dateFragment.setTargetFragment(AddPostFragment.this, DATE_REQUEST_CODE);
-            // Show DatePickerFragment
-            dateFragment.show(getFragmentManager(), "DatePicker");
-        });
-
-        // Launch DatePicker on Date button press
-        dateET.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Create DatePickerFragment
-                DialogFragment dateFragment = new DatePickerFragment();
-                // Set the target fragment to receive the results and specifying the result code
-                dateFragment.setTargetFragment(AddPostFragment.this, DATE_REQUEST_CODE);
-                // Show DatePickerFragment
-                dateFragment.show(getFragmentManager(), "DatePicker");
-            }
-        });
-
-        // Launch TimePicker on Time button press
-        timeButton.setOnClickListener(view -> {
-            // Create DatePickerFragment
-            DialogFragment timeFragment = new TimePickerFragment();
-            // Set the target fragment to receive the results and specifying the result code
-            timeFragment.setTargetFragment(AddPostFragment.this, TIME_REQUEST_CODE);
-            // Show TimePickerFragment
-            timeFragment.show(getFragmentManager(), "TimePicker");
-        });
-
-        // Launch DatePicker on Date button press
-        timeET.setOnClickListener(view -> {
-            // Create DatePickerFragment
-            DialogFragment dateFragment = new DatePickerFragment();
-            // Set the target fragment to receive the results and specifying the result code
-            dateFragment.setTargetFragment(AddPostFragment.this, DATE_REQUEST_CODE);
-            // Show DatePickerFragment
-            dateFragment.show(getFragmentManager(), "DatePicker");
-        });
-
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Clear entered fields
-                dateET.setText("");
-                timeET.setText("");
-                descET.setText("");
-            }
-        });
-
-        doneButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
+        dateET.setOnClickListener(this);
+        timeET.setOnClickListener(this);
+        cancelButton.setOnClickListener(this);
+        doneButton.setOnClickListener(this);
 
         return root;
     }
+
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -126,5 +70,40 @@ public class AddPostFragment extends Fragment {
             // Set time EditText to the selected time
             timeET.setText(selectedTime);
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()){
+
+            case R.id.date_edittext:
+                // Create DatePickerFragment
+                DialogFragment dateFragment = new DatePickerFragment();
+                // Set the target fragment to receive the results and specifying the result code
+                dateFragment.setTargetFragment(AddPostFragment.this, DATE_REQUEST_CODE);
+                // Show DatePickerFragment
+                dateFragment.show(getFragmentManager(), "DatePicker");
+                Log.d("Test", "Date text pressed");
+                break;
+            case R.id.time_edittext:
+                // Create TimePickerFragment
+                DialogFragment timeFragment = new TimePickerFragment();
+                // Set the target fragment to receive the results and specifying the result code
+                timeFragment.setTargetFragment(AddPostFragment.this, TIME_REQUEST_CODE);
+                // Show TimePickerFragment
+                timeFragment.show(getFragmentManager(), "TimePicker");
+                break;
+            case R.id.cancel_button:
+                // Clear entered fields
+                dateET.setText("");
+                timeET.setText("");
+                descET.setText("");
+                Log.d("Test", "Cancel button pressed");
+                break;
+            case R.id.done_button:
+                break;
+        }
+
     }
 }
