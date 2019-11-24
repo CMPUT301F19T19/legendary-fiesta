2d82f88a255f38ffb32b2c8ffe97f182176779bd
Lefan <tan6@ualberta.ca>
2019-11-23 21:29:48 -0700

    UI changes to friends layout

--- app/src/main/java/io/github/cmput301f19t19/legendary_fiesta/ui/FriendsFragment.java
+++ app/src/main/java/io/github/cmput301f19t19/legendary_fiesta/ui/FriendsFragment.java
@@ -198,6 +198,7 @@ public class FriendsFragment extends Fragment implements  View.OnClickListener, 
                     // no-op because no deleting searches
                 }
             });
+            //friendsListView.setPadding();
             friendsListView.setAdapter(requestArrayAdapter);
         }
 
